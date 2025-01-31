d889b878ec0d7553a50280bef3789d984742788b
rehamanshaik234 <115864306+rehamanshaik234@users.noreply.github.com>
2025-01-31 10:48:04 +0530

    Update PRApprovalMethod.js

--- PRApprovalMethod.js
+++ PRApprovalMethod.js
@@ -25,7 +25,7 @@ const USERNAME = 's23hana3';
 const PASSWORD = 'Best@12345';
 
 admin.initializeApp({
-  credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS),
+  credential: admin.credential.cert(JSON.pars(process.env.GOOGLE_APPLICATION_CREDENTIALS)),
 });
 const db = admin.firestore(); 
 
